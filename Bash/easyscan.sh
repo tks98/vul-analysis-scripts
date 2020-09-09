d6b4d4163532ee56d6566db08fecfb88c7b4f8b2
tsuika <tsuika98@gmail.com>
2020-09-08 21:38:43 -0500

    add scan time to bash scripts

--- Bash/easyscan.sh
+++ Bash/easyscan.sh
@@ -10,6 +10,8 @@ read host
 echo "Scanning host ports from 1..1024"
 
 # scan ports
+# start timer, need %N for more accurate time
+start=$(date +%s.%N)
 for port in {1..1024}; do
     nc -z $host $port; # run netcat, if result is 0, port is open
     if [ $? -eq 0 ]
@@ -17,3 +19,7 @@ for port in {1..1024}; do
         echo "Port: $port is open"
     fi
 done
+
+# calculate time taken
+finish=$(echo "$(date +%s.%N) - $start" | bc) # pipe expression into bc calculator to get runtime
+echo "Scan took $finish seconds"
--- Bash/scan.sh
+++ Bash/scan.sh
@@ -10,9 +10,15 @@ read host
 echo "Scanning host ports from 1..1024"
 
 # scan ports
+# start timer
+start=$(date +%s.%N)
 for port in {1..1024}; do
-  if 2> /dev/null > /dev/tcp/$host/$port; # re-direct stderr (2) to dev/null, # run dev/tcp, if port is open, result is true, echo open port
+  if 2> /dev/null > /dev/tcp/$host/$port; # re-direct stderr (2) to dev/null, run dev/tcp, if port is open, result is true, echo open port
   then
     echo "port $port is open"
   fi
 done
+
+# calculate time taken
+finish=$(echo "$(date +%s.%N) - $start" | bc) # pipe expression into bc calulator to get runtime
+echo "Scan took $finish seconds"
